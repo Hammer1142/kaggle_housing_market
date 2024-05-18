4a67acbbb23de42b8afd1f32c3bfcad0bc7880ec
Hammer1142 <fabian.hammerschmid@icloud.com>
2024-05-18 16:59:18 +0200

    First commit

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name = '',
+    name = 'Kaggle Housing Prices',
     version = '0.0.1',
     author = 'Fabian Hammerschmid',
     author_email= 'fabian.hammerschmid@gmail.com',
